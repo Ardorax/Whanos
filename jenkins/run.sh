ba13dd736e4f0fc79e5b0d28629b28981f4985b9
ImperialCrowns <maxime.dziura@epitech.eu>
2023-11-29 13:59:46 +0100

    fix: jenkins.yml

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,1 +1,1 @@
-docker run --name jenkins --rm -v `pwd`:/var/jenkinsData -p 8080:8080 jenkins:custumJenkins
+docker run --name jenkins --rm -v `pwd`:/var/jenkinsData -p 8080:8080 --env ADMIN_PASSWORD=admin jenkins:custumJenkins
